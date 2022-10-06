c307d4d50fa3ad81cbe18db6cecd858e7d2ecfac
andmatcha <jj.mcr185@gmail.com>
2022-10-06 14:42:46 +0900

    feat: :sparkles: レイアウト作成

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,28 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import { ReactNode } from "react";
+import { css } from "@emotion/react";
+import Header from "../objects/common/Header";
+
+interface Props {
+  title: string;
+  children: ReactNode;
+}
+
+const Layout: NextPage<Props> = ({ title, children }) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content="This is a sample page." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <main>{children}</main>
+    </>
+  );
+};
+
+const styles = {};
+
+export default Layout;
