9c2c6722fdb705700cac57bbc8bb513db3460505
andmatcha <jj.mcr185@gmail.com>
2022-10-06 15:18:33 +0900

    feat: :sparkles: ヘッダーの内容を調整

--- components/objects/common/Header.tsx
+++ components/objects/common/Header.tsx
@@ -5,9 +5,11 @@ const Header = () => {
     <header css={styles.header}>
       <h1 css={styles.title}>ARES Project</h1>
       <ul css={styles.menu}>
-        <li css={styles.menuItem}>ABOUT US</li>
-        <li css={styles.menuItem}>ROVERS</li>
         <li css={styles.menuItem}>NEWS</li>
+        <li css={styles.menuItem}>ABOUT US</li>
+        <li css={styles.menuItem}>OUR ROVERS</li>
+        <li css={styles.menuItem}>BLOG</li>
+        <li css={styles.menuItem}>CONTACT</li>
       </ul>
     </header>
   );
