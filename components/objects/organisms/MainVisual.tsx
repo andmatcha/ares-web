966ab4a503624075d452ea63de96d0c551ba7317
andmatcha <jj.mcr185@gmail.com>
2022-11-01 05:08:59 +0900

    fix: :pencil2: 文言を修正、その他

--- components/objects/organisms/MainVisual.tsx
+++ components/objects/organisms/MainVisual.tsx
@@ -11,18 +11,18 @@ const MainVisual: NextPage = () => {
         />
       </div>
       <div className="absolute top-0 left-0 w-full h-full bg-main-bg/75">
-        <div className="w-full h-full flex flex-col justify-end gap-2 md:gap-5 pl-2 md:pl-20 pb-[40vh] md:pb-20">
+        <div className="w-full h-full flex flex-col justify-end gap-2 pl-2 md:pl-20 pb-[40vh] md:pb-20">
           <h2 className="text-4xl md:text-6xl">
             We are the A<span className="text-red-600">R</span>ES.
           </h2>
-          <p className="text-xl md:text-3xl">世界に通用する火星ローバーを日本から</p>
-          <p className="text-base md:text-xl max-w-xl leading-8 tracking-wider">
+          <p className="text-xl md:text-3xl">世界レベルの火星ローバーを日本から</p>
+          <p className="mt-4 text-base md:text-xl max-w-xl leading-8 tracking-wider">
             私たちは世界有数のローバーのコンテストであるURC(University Rover
             Challenge)の日本勢初出場を目指して活動しています。
           </p>
         </div>
       </div>
-      <div className="absolute bottom-20 left-5 md:right-20 animate-arrow-up-down">
+      <div className="absolute bottom-20 left-5 md:left-auto md:right-20 animate-arrow-up-down">
         <svg
           className="w-20 h-20"
           fill="none"
