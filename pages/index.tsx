0380990ce6c6f9cc9b32b1b63243467971a497d7
andmatcha <jj.mcr185@gmail.com>
2022-11-01 00:21:01 +0900

    feat: :sparkles: paragraphコンポーネントを作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import MainVisual from "../components/objects/organisms/MainVisual";
 import Section from "../components/objects/molecules/Section";
 import MainButton from "../components/objects/atoms/MainButton";
 import NewsRow from "../components/objects/molecules/NewsRow";
+import Paragraph from "../components/objects/atoms/Paragraph";
 import dayjs from "dayjs";
 import Image from "next/image";
 
@@ -14,24 +15,25 @@ const Home: NextPage = () => {
       <div css={styles.container}>
         <MainVisual></MainVisual>
         <Section title="TEAM">
-          <div css={styles.about}>
-            <div css={styles.imageBox}>
-              <Image
-                src="/images/logo.jpg"
-                alt=""
-                width={560}
-                height={560}
-                objectFit="cover"
-                priority={true}
-              />
-            </div>
-            <p css={styles.description}>
-              観測ロケット「MOMO
-              -モモ-」は弾道飛行を行うロケットです。様々な観測機器やお客様からお預かりした荷物をペイロードとして搭載し、宇宙空間に到達します。微小重力環境での科学実験に使われているほか、企業のPRやブランディング利用の市場も開拓しています。
-            </p>
+          <div className="flex justify-center gap-[10%]">
+            <img
+              src="/images/rover2.png"
+              alt=""
+              className="w-[45%]"
+            />
+            <Paragraph className="w-[45%]">
+              <h4 className="text-2xl">東京班</h4>
+              東京班は主にアームの設計を行っています。この大会では宇宙飛行士のサポートを前提としたミッションや岩を移動させるミッションがあるためアームは重要な役割を果たします。
+            </Paragraph>
           </div>
         </Section>
-        <Section title="WHAT'S URC?"></Section>
+        <Section title="WHAT'S URC?">
+          <Paragraph>
+            University Rober
+            Challengeは大学生を対象とした世界最高峰のロボット工学コンテストです。
+            URCは、毎年米国ユタ州南部の砂漠で開催され、それぞれのチームが、火星を調査する宇宙飛行士と一緒に働く次世代の火星探査機の設計と製作に挑戦しています。
+          </Paragraph>
+        </Section>
         <Section title="ROVERS">
           <div className="flex flex-col items-center">
             <MainButton text="SEE OUR ROVERS" />
