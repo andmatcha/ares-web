8c7f457e18ff0f90136563e6ee68db339de5769a
andmatcha <jj.mcr185@gmail.com>
2022-10-31 22:33:41 +0900

    refactor: :recycle: tailwindに乗り換え

--- components/objects/molecules/Section.tsx
+++ components/objects/molecules/Section.tsx
@@ -1,29 +1,13 @@
-import { css } from "@emotion/react";
 import { NextPage } from "next";
 import { ReactNode } from "react";
 
 const Section: NextPage<{ title: string; children?: ReactNode }> = ({ title, children }) => {
   return (
-    <section css={styles.section}>
+    <section className="h-screen py-20 px-[10%] flex flex-col items-center gap-10 text-lg">
       <h3 className="text-4xl mt-10 tracking-wider">{title}</h3>
-      <div css={styles.content}>{children}</div>
+      <div className="w-full">{children}</div>
     </section>
   );
-};
-
-const styles = {
-  section: css`
-    height: 100vh;
-    padding: 80px 10%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 40px;
-    font-size: 18px;
-  `,
-  content: css`
-    width: 100%;
-  `
 };
 
 export default Section;
