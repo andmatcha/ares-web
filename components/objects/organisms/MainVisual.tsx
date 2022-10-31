e12eabc03df68acf4035bfe278c0b163713149d3
andmatcha <jj.mcr185@gmail.com>
2022-11-01 00:18:34 +0900

    feat: :sparkles: メインビジュアルに矢印を追加

--- components/objects/organisms/MainVisual.tsx
+++ components/objects/organisms/MainVisual.tsx
@@ -24,6 +24,22 @@ const MainVisual: NextPage = () => {
           </p>
         </div>
       </div>
+      <div className="absolute bottom-20 right-20 animate-arrow-up-down">
+        <svg
+          className="w-20 h-20"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M19 13l-7 7-7-7m14-8l-7 7-7-7"
+          ></path>
+        </svg>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,17 @@ module.exports = {
     extend: {
       colors: {
         'main-bg': '#1a1a1a'
-      }
+      },
+      keyframes: {
+        'up-down': {
+          '0%': { transform: 'translateY(0)', opacity: 0.4 },
+          '45%': { transform: 'translateY(-40px)', opacity: 0 },
+          '100%': { transform: 'translateY(0)', opacity: 0.4 },
+        },
+      },
+      animation: {
+        'arrow-up-down': 'up-down 5s infinite',
+      },
     },
   },
   plugins: [],
