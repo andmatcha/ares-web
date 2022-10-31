5153b4981245e5d2e2f465c499bf8bf7b098d76c
andmatcha <jj.mcr185@gmail.com>
2022-11-01 00:39:41 +0900

    feat: :sparkles: トップページにコンテンツを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,25 +14,35 @@ const Home: NextPage = () => {
     <Layout title="ARES Project">
       <div css={styles.container}>
         <MainVisual></MainVisual>
-        <Section title="TEAM">
+        <Section title="TEAM" className="flex flex-col gap-20">
           <div className="flex justify-center gap-[10%]">
-            <img
-              src="/images/rover2.png"
-              alt=""
-              className="w-[45%]"
-            />
-            <Paragraph className="w-[45%]">
+            <img src="/images/rover2.png" alt="" className="w-[45%]" />
+            <div className="w-[45%]">
               <h4 className="text-2xl">東京班</h4>
-              東京班は主にアームの設計を行っています。この大会では宇宙飛行士のサポートを前提としたミッションや岩を移動させるミッションがあるためアームは重要な役割を果たします。
-            </Paragraph>
+              <Paragraph>
+                東京班は主にアームの設計を行っています。この大会では宇宙飛行士のサポートを前提としたミッションや岩を移動させるミッションがあるためアームは重要な役割を果たします。
+              </Paragraph>
+            </div>
+          </div>
+          <div className="flex justify-center gap-[10%]">
+            <div className="w-[45%]">
+              <h4 className="text-2xl">東京班</h4>
+              <Paragraph>
+                東京班は主にアームの設計を行っています。この大会では宇宙飛行士のサポートを前提としたミッションや岩を移動させるミッションがあるためアームは重要な役割を果たします。
+              </Paragraph>
+            </div>
+            <img src="/images/arm2.png" alt="" className="w-[45%]" />
           </div>
         </Section>
         <Section title="WHAT'S URC?">
-          <Paragraph>
-            University Rober
-            Challengeは大学生を対象とした世界最高峰のロボット工学コンテストです。
-            URCは、毎年米国ユタ州南部の砂漠で開催され、それぞれのチームが、火星を調査する宇宙飛行士と一緒に働く次世代の火星探査機の設計と製作に挑戦しています。
-          </Paragraph>
+          <div className="flex gap-[4%]">
+            <img src="/images/mars1.png" alt="" className="w-[48%]" />
+            <Paragraph className="w-[48%]">
+              University Rober
+              Challengeは大学生を対象とした世界最高峰のロボット工学コンテストです。
+              URCは、毎年米国ユタ州南部の砂漠で開催され、それぞれのチームが、火星を調査する宇宙飛行士と一緒に働く次世代の火星探査機の設計と製作に挑戦しています。
+            </Paragraph>
+          </div>
         </Section>
         <Section title="ROVERS">
           <div className="flex flex-col items-center">
