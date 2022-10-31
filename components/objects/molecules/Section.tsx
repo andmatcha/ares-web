9a901e9fc8eb04690d3a387a00051f74f50c25cb
andmatcha <jj.mcr185@gmail.com>
2022-11-01 00:20:23 +0900

    fix: :sparkles: セクションが長くなった場合に対応

--- components/objects/molecules/Section.tsx
+++ components/objects/molecules/Section.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 
 const Section: NextPage<{ title: string; children?: ReactNode }> = ({ title, children }) => {
   return (
-    <section className="h-screen py-20 px-[10%] flex flex-col items-center gap-10 text-lg">
+    <section className="min-h-screen py-20 px-[10%] flex flex-col items-center gap-10 text-lg">
       <h3 className="text-4xl mt-10 tracking-wider">{title}</h3>
       <div className="w-full">{children}</div>
     </section>
