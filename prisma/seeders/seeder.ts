92d5a06adcc37a438c70f31f7e0918c0e0163537
andmatcha <jj.mcr185@gmail.com>
2023-01-10 13:26:37 +0900

    refactor: :recycle: 細かい修正

--- prisma/seeders/seeder.ts
+++ prisma/seeders/seeder.ts
@@ -1,16 +1,6 @@
-import { PrismaClient, Prisma } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import TagSeeder from "./tagSeeder.js";
 const prisma = new PrismaClient();
-
-// モデル投入用のデータ定義
-const tagInputs: Prisma.TagCreateInput[] = [
-  { name: "慶應班" },
-  { name: "東北班" },
-  { name: "機構班" },
-  { name: "電装班" },
-  { name: "BIO班" },
-  { name: "制御班" },
-];
 
 const seed = async () => {
   TagSeeder();
