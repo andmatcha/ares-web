954dd2e96ddae28e966f8ea27296ec66c16abf50
フルヤミウ <miyamiu19642002@gmail.com>
2023-05-16 00:30:30 +0900

    teamContents構成

--- contents/team.ts
+++ contents/team.ts
@@ -1,11 +1,11 @@
 const teamContents = {
   title: "チーム紹介",
-  // branches: [
-    // {
-    //   title: "機構班",
-    //   imagePath: "/images/keio_members.jpg",
-    //   paragraph:
-    //     "班の説明。",
+  branches: [
+    {
+      title: "機構班",
+      imagePath: "/images/keio_members.jpg",
+      paragraph:
+        "班の説明。",
       members: [
         {
           imagePath: "/images/arm1.png",
@@ -20,14 +20,28 @@ const teamContents = {
           name: "cccさん"
         },
       ],
-    // },
-    // {
-    //   title: "東北班",
-    //   imagePath: "/images/tohoku_members.jpg",
-    //   paragraph:
-    //     "東北班は機体の設計、開発を行っています。整備されていない地形でミッションを進めることができるようにそれぞれの得意分野を活かして開発しています。",
-    // },
-  // ],
+    },
+    {
+      title: "東北班",
+      imagePath: "/images/tohoku_members.jpg",
+      paragraph:
+        "東北班は機体の設計、開発を行っています。整備されていない地形でミッションを進めることができるようにそれぞれの得意分野を活かして開発しています。",
+      members: [
+        {
+          imagePath: "/images/arm1.png",
+          name: "aaaさん"
+        },
+        {
+          imagePath: "/images/arm2.png",
+          name: "bbbさん"
+        },
+        {
+          imagePath: "/images/arm3.png",
+          name: "cccさん"
+        },
+      ],
+    },
+  ],
 };
 
 export default teamContents;
--- pages/team.tsx
+++ pages/team.tsx
@@ -7,48 +7,47 @@ import topContents from "../contents/top";
 import teamContents from "../contents/team";
 
 const Team: NextPage = () => {
-
   return (
     <Layout title={contents.title}>
-    <div className="w-full h-20"></div>
-     <Section
-       id={teamContents.title}
-       title={teamContents.title}
-       className="flex flex-col gap-20"
-     >
-     <div className="flex flex-col justify-center items-center gap-4">
-     <img
-        src="/images/keio_members.jpg"
-        alt="ARES"
-        className="w-1/6 md:w-3/5"
-      />
-      <Paragraph className="md:w-3/5 md:text-center">
-           {/* {topContents.team.paragraph} */}
-           texttexttexttexttexttexttextt
-           exttexttexttexttexttexttext
-         </Paragraph>  
-        </div> 
-        <div className="flex flex-row justify-center">
-        {teamContents.members.map((member, index) => (
-           <div
-            key={index}
-            className={`flex ${
-              index % 2 === 0
-                ? "flex-col md:flex-row"
-                : "flex-col md:flex-row-reverse"
-            } justify-center items-center gap-4 md:gap-[10%]`}
-          >
-     
-        {/* <img src={member.imagePath} alt="" /> */}
-        <Paragraph><div >{member.name}</div></Paragraph>
+      <div className="w-full h-20"></div>
+      <Section
+        id={teamContents.title}
+        title={teamContents.title}
+        className="flex flex-col gap-20"
+      >
+        <div className="flex flex-col justify-center items-center gap-4">
+          <img
+            src="/images/keio_members.jpg"
+            alt="ARES"
+            className="w-1/6 md:w-3/5"
+          />
+          <Paragraph className="md:w-3/5 md:text-center">
+            {/* {topContents.team.paragraph} */}
+            texttexttexttexttexttexttextt exttexttexttexttexttexttext
+          </Paragraph>
         </div>
-      ))}
-      </div>
-     </Section>
-
-     </Layout>
-
-  )
+        {teamContents.branches.map((branch, index) => (
+          <div key={index}>
+            {branch.title}
+            <div className="flex justify-center gap-20">
+              {branch.members.map((member, memberIndex) => (
+                <div
+                  key={memberIndex}
+                  className={`flex justify-center items-center gap-4 md:gap-[10%]`}
+                >
+                  <div>
+                    <img src={member.imagePath} alt="" />
+                    <div className="text-center">{member.name}</div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+        
+      </Section>
+    </Layout>
+  );
 };
 
 export default Team;
