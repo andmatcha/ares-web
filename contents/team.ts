0cd05bb58cd2bc14e2fbcec4ad6a9683c2e0c976
フルヤミウ <miyamiu19642002@gmail.com>
2023-05-15 22:15:24 +0900

    メンバー紹介作成途中

--- contents/team.ts
+++ contents/team.ts
@@ -1,5 +1,33 @@
 const teamContents = {
   title: "チーム紹介",
+  // branches: [
+    // {
+    //   title: "機構班",
+    //   imagePath: "/images/keio_members.jpg",
+    //   paragraph:
+    //     "班の説明。",
+      members: [
+        {
+          imagePath: "/images/arm1.png",
+          name: "aaaさん"
+        },
+        {
+          imagePath: "/images/arm2.png",
+          name: "bbbさん"
+        },
+        {
+          imagePath: "/images/arm3.png",
+          name: "cccさん"
+        },
+      ],
+    // },
+    // {
+    //   title: "東北班",
+    //   imagePath: "/images/tohoku_members.jpg",
+    //   paragraph:
+    //     "東北班は機体の設計、開発を行っています。整備されていない地形でミッションを進めることができるようにそれぞれの得意分野を活かして開発しています。",
+    // },
+  // ],
 };
 
 export default teamContents;
--- pages/team.tsx
+++ pages/team.tsx
@@ -1,7 +1,54 @@
 import { NextPage } from "next";
+import Layout from "../components/layouts/Layout";
+import contents from "../contents/team";
+import Section from "../components/objects/molecules/Section";
+import Paragraph from "../components/objects/atoms/Paragraph";
+import topContents from "../contents/top";
+import teamContents from "../contents/team";
 
 const Team: NextPage = () => {
-  return <div></div>;
+
+  return (
+    <Layout title={contents.title}>
+    <div className="w-full h-20"></div>
+     <Section
+       id={teamContents.title}
+       title={teamContents.title}
+       className="flex flex-col gap-20"
+     >
+     <div className="flex flex-col justify-center items-center gap-4">
+     <img
+        src="/images/keio_members.jpg"
+        alt="ARES"
+        className="w-1/6 md:w-3/5"
+      />
+      <Paragraph className="md:w-3/5 md:text-center">
+           {/* {topContents.team.paragraph} */}
+           texttexttexttexttexttexttextt
+           exttexttexttexttexttexttext
+         </Paragraph>  
+        </div> 
+        <div className="flex flex-row justify-center">
+        {teamContents.members.map((member, index) => (
+           <div
+            key={index}
+            className={`flex ${
+              index % 2 === 0
+                ? "flex-col md:flex-row"
+                : "flex-col md:flex-row-reverse"
+            } justify-center items-center gap-4 md:gap-[10%]`}
+          >
+     
+        {/* <img src={member.imagePath} alt="" /> */}
+        <Paragraph><div >{member.name}</div></Paragraph>
+        </div>
+      ))}
+      </div>
+     </Section>
+
+     </Layout>
+
+  )
 };
 
 export default Team;
