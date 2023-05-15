3676af32c321afa1aadf3469674e9d2a8bf0ddfe
andmatcha <jj.mcr185@gmail.com>
2023-05-16 03:56:22 +0900

    fix: :lipstick: スポンサーエリアをコンポーネント化、表示を増やした

--- const/sponsors.ts
+++ const/sponsors.ts
@@ -4,12 +4,12 @@ const sponsors = [
     imagePath: "/images/brand_marks/ablab.png",
   },
   {
-    name: "マブチモーター",
-    imagePath: "/images/brand_marks/mabuchi.jpg",
-  },
-  {
     name: "工機ホールディングス",
     imagePath: "/images/brand_marks/hikoki.jpg",
+  },
+  {
+    name: "マブチモーター",
+    imagePath: "/images/brand_marks/mabuchi.jpg",
   },
 ];
 
