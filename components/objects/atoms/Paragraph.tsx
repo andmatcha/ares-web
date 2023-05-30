2b5a1e2f6295c2035c36753e27f2696592446c86
andmatcha <jj.mcr185@gmail.com>
2023-05-31 01:41:02 +0900

    style: :lipstick: UI調整

--- components/objects/atoms/Paragraph.tsx
+++ components/objects/atoms/Paragraph.tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next";
 import React, { ReactNode } from "react";
 
 const Paragraph: NextPage<{ children?: ReactNode, className?: string }> = ({ children, className }) => {
-  return <div className={`tracking-widest text-sm md:text-base leading-6 md:leading-8 ${className}`}>{children}</div>;
+  return <div className={`tracking-widest text-xs md:text-sm leading-6 md:leading-8 ${className}`}>{children}</div>;
 };
 
 export default Paragraph;
--- components/objects/molecules/Section.tsx
+++ components/objects/molecules/Section.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 
 const Section: NextPage<{ title?: string; id: string; children?: ReactNode, className?: string }> = ({ title, id, children, className }) => {
   return (
-    <section id={id} className="py-10 md:py-20 px-2 md:px-[10%] flex flex-col items-center gap-10 text-lg">
+    <section id={id} className="py-5 md:py-16 px-2 md:px-[10%] flex flex-col items-center gap-5 md:gap-10 text-lg">
       {title && <h3 className="text-3xl md:text-4xl mt-10 tracking-wider">{title}</h3>}
       <div className={`w-full ${className}`}>{children}</div>
     </section>
