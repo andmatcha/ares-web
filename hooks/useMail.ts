3990e606685039e742550079e280cf7a30409726
andmatcha <jj.mcr185@gmail.com>
2024-02-14 12:59:59 +0900

    feat: :sparkles: 問い合わせ元にメール送信

--- hooks/useMail.ts
+++ hooks/useMail.ts
@@ -1,12 +1,24 @@
 export const useMail = () => {
-  const send = async (name: string, email: string, message: string) => {
-    await fetch("/api/mail", {
+  const sendToAres = async (name: string, email: string, message: string) => {
+    await fetch("/api/mail/to-ares", {
+      method: "POST",
+      body: JSON.stringify({ name, email, message }),
+    });
+  };
+
+  const sendToCustomer = async (
+    name: string,
+    email: string,
+    message: string
+  ) => {
+    await fetch("/api/mail/to-ares", {
       method: "POST",
       body: JSON.stringify({ name, email, message }),
     });
   };
 
   return {
-    send,
+    sendToAres,
+    sendToCustomer,
   };
 };
