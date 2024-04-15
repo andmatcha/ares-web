3cd5d504fe984acd3a477d2b015d8ad896da2f48
andmatcha <jj.mcr185@gmail.com>
2024-04-15 23:31:01 +0900

    refactor: :truck: fonts.ts移動

---
+++ app/_common/utils/font.ts
@@ -0,0 +1,19 @@
+import { Montserrat, Noto_Sans_JP, Orbitron } from "next/font/google"; // google fonts
+
+// 全ページ共通のフォント
+// 英語フォント
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
+
+// 日本語フォント
+export const noto_sans_jp = Noto_Sans_JP({
+  variable: "--font-noto-sans-jp",
+});
+
+// 角張った英語フォント(タイトルやヘッダーなどに使用)
+export const orbitron = Orbitron({
+  subsets: ["latin"],
+  variable: "--font-orbitron",
+});
