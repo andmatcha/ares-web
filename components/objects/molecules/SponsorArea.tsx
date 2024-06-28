97ecabe8b20946dd0408b91dc041f8fa6068e6c5
andmatcha <jj.mcr185@gmail.com>
2024-06-28 22:43:31 +0900

    feat: :construction: スポンサー表示サイズ分け 途中

--- components/objects/molecules/SponsorArea.tsx
+++ components/objects/molecules/SponsorArea.tsx
@@ -1,27 +1,59 @@
 import { NextPage } from "next";
 
-import sponsors from "../../../const/sponsors";
+import { RANK, sponsors } from "../../../const/sponsors";
+import { Sponsor } from "../../../types";
 
 const SponsorArea: NextPage = () => {
-  return (
-    <div className="py-10 bg-black">
-      <div className="flex flex-wrap justify-center items-center gap-x-6 gap-y-4 md:gap-12 px-8">
-        {sponsors.map(({ name, imagePath, url }, index) => (
-          <a
-            key={index}
-            href={url}
-            target="blank"
-            className="w-32 md:w-48 h-20 md:h-24 flex items-center"
-          >
-            <img
-              src={imagePath}
-              alt={name}
-              className="w-full h-full object-contain"
-            />
-          </a>
-        ))}
+  const sponsorArea = (
+    sponsorList: Sponsor[],
+    size: "small" | "medium" | "large"
+  ) => {
+    const sizeClass = () => {
+      switch (size) {
+        case "small":
+          return "w-32 md:w-48 h-20 md:h-24";
+        case "medium":
+          return "w-48 md:w-64 h-24 md:h-32";
+        case "large":
+          return "w-64 md:w-80 h-32 md:h-40";
+      }
+    };
+    return (
+      <div className="py-10 bg-black">
+        <div className="flex flex-wrap justify-center items-center gap-x-6 gap-y-4 md:gap-12 px-8">
+          {sponsorList.map(({ name, imagePath, url }, index) => (
+            <a
+              key={index}
+              href={url}
+              target="blank"
+              className={`${sizeClass()} flex items-center`}
+            >
+              <img
+                src={imagePath}
+                alt={name}
+                className="w-full h-full object-contain"
+              />
+            </a>
+          ))}
+        </div>
       </div>
-    </div>
+    );
+  };
+  return (
+    <>
+      {sponsorArea(
+        sponsors.filter((sponsor) => sponsor.rank === RANK.DIAMOND),
+        "large" // 51200
+      )}
+      {sponsorArea(
+        sponsors.filter((sponsor) => sponsor.rank === RANK.GOLD),
+        "medium" // 32768
+      )}
+      {sponsorArea(
+        sponsors.filter((sponsor) => sponsor.rank === RANK.OTHER),
+        "small" //18432
+      )}
+    </>
   );
 };
 
