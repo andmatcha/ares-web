fe5101e9252a64c8de565da8c1fa8b0be6a5c129
andmatcha <jj.mcr185@gmail.com>
2024-10-11 20:26:34 +0900

    fix: :bug: importした

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -1,19 +1,13 @@
 import { NextPage } from "next";
 
-import HyakuBanch from "../objects/molecules/HyakuBanch";
 import SponsorArea from "../objects/molecules/SponsorArea";
 
-const Footer: NextPage<{ withSponsor?: boolean }> = ({ withSponsor = false }) => {
+const Footer: NextPage<{ withSponsor?: boolean }> = ({
+  withSponsor = false,
+}) => {
   return (
     <footer className="w-full">
-      <div>
-        {withSponsor && (
-          <>
-            <SponsorArea />
-            {/* <HyakuBanch /> */}
-          </>
-        )}
-      </div>
+      <div>{withSponsor && <SponsorArea />}</div>
       <div className="w-full flex flex-col items-center bg-black">
         <p className="w-full text-sm text-center py-2 text-gray-500 tracking-wider">
           &copy; 2022 ARES Project
--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import React from "react";
 import { ReactNode } from "react";
 
 import Footer from "./Footer";
